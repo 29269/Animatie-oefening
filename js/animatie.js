c39089c262c5610bf62cfcd39517e6819dad6aa4
Duneya <29269@ma-web.nl>
2020-12-04 12:34:17 +0100

    stap 6

--- js/animatie.js
+++ js/animatie.js
@@ -6,3 +6,15 @@ document.querySelector('.animeer-knop').addEventListener('click', (e) =>{
 setTimeout(() => {
     document.querySelector('.vlakje').classList.toggle('vlakje__naar-recht')
 }, 2000);
+
+//een klik op de menu knop laat de link__link (alle) na elkaar binnen schuiven
+//door ze de class nav__link--schuif
+const menuKnop = document.querySelector('.nav__knop');
+const  navgatieItemas = document.querySelectorAll('.nav__link');
+menuKnop.addEventListener('click', () =>{
+    navgatieItemas.forEach((item,index) => {
+        setTimeout(() => {
+            item.classList.toggle('nav__link--schuif-in');
+        }, 100* index);
+    })
+});
